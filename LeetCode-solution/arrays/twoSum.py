17e76069193405b2b3ce370f8faf3cf175ea1906
IannisG10 <144343272+IannisG10@users.noreply.github.com>
2024-10-31 15:04:23 +0100

    Add files via upload

---
+++ LeetCode-solution/arrays/twoSum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self,nums,target):
+        l = len(nums)
+        for i in range(l):
+            for j in range(i+1, l):
+                if(nums[i]+nums[j] == target):
+                    return [i, j]
+
+
+
+        
+        
+        
