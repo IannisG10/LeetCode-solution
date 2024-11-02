78d88e147179f6302f2693795c84ec0bf94c1c72
IannisG10 <144343272+IannisG10@users.noreply.github.com>
2024-11-02 09:27:25 +0300

    Update example.py

--- LeetCode-solution/arrays/example.py
+++ LeetCode-solution/arrays/example.py
@@ -1,1 +1,16 @@
+class Solution:
+  def twoSum(self,array,target):
+    l = len(array)
+    for i in range(l):
+      for j in range(i+1,l):
+        if arr[i]+arr[j] == target:
+          print(f"[{i},{j}]")
+          return 
 
+arr_input = [2,7,11,15]
+target = 9
+exec_Solution = Solution()
+exec_Solution.twoSum(arr_input,target)
+      
+    
+  
