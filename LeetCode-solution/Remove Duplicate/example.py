d253f641640506d389e30588ddfff8b964c7850c
IannisG10 <144343272+IannisG10@users.noreply.github.com>
2024-11-03 17:46:30 +0300

    Update example.py

--- LeetCode-solution/Remove Duplicate/example.py
+++ LeetCode-solution/Remove Duplicate/example.py
@@ -1,1 +1,12 @@
-
+class Solution:
+  def remove_duplicate(self,array):
+    l = len(array)
+    j= 1 #index du premier élément unique
+    for i in range(1,l):
+      if array[i] != array[i-1]:
+        array[j]=array[i]
+        j+=1 
+      else:
+        continue
+    
+    return j
