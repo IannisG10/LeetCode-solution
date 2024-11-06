fad252ffad13750c28398bfc8ce9fdcf79a144b7
IannisG10 <144343272+IannisG10@users.noreply.github.com>
2024-11-06 08:34:23 +0100

    Update remove-element.py

--- LeetCode-solution/Remove Element/remove-element.py
+++ LeetCode-solution/Remove Element/remove-element.py
@@ -1,1 +1,14 @@
+class Solution: 
+    def removeElement(self,nums,val):
+        l = len(nums)
+        j = 0
+        for i in range(l):
+            if nums[i] != val:
+                nums[j] = nums[i]
+                j += 1
+            else:
+                continue
+        
+        return j
 
+        
