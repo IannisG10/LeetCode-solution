26510a3467a85ea57c49ab8f94fcfeaa093114f5
IannisG10 <144343272+IannisG10@users.noreply.github.com>
2024-11-06 08:43:34 +0100

    Update example.py

--- LeetCode-solution/Remove Element/example.py
+++ LeetCode-solution/Remove Element/example.py
@@ -1,1 +1,25 @@
+class Exercise:
+  def remove_element(self,arr,val):
+    l = len(arr)
+    j = 0 #index du 1er élément
+    
+    for i in range(l):
+      if arr[i] != val:
+        arr[j] = arr[i]
+        j += 1 
+      else:
+        continue 
+      
+    return j 
+    
+array = [1,3,2,2,3,1,2,4,3]
+exepect_arr = []
+value = 3
 
+element = Exercise().remove_element(array,value)
+
+for i in range(element):
+  exepect_arr.append(array[i])
+
+print(f"Number of element: {element}")
+print(exepect_arr)
