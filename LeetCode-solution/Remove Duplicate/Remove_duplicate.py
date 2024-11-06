dd4b75ea25570312b011435cfa90ae6648da2544
IannisG10 <144343272+IannisG10@users.noreply.github.com>
2024-11-06 07:45:34 +0100

    Update Remove_duplicate.py

--- LeetCode-solution/Remove Duplicate/Remove_duplicate.py
+++ LeetCode-solution/Remove Duplicate/Remove_duplicate.py
@@ -1,3 +1,10 @@
 class Solution:
-  def remove_duplicate(self,arr):
-    l = len(arr)
+    def removeDuplicates(self,nums):
+        l = len(nums)
+        j = 1
+        for i in range(1,l):
+            if nums[i] != nums[i-1]:
+                nums[j] = nums[i]
+                j += 1
+
+        return j
