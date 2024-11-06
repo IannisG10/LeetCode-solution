a07998a5d50e8eef7ebbbdfe90f6f2c55065407a
IannisG10 <144343272+IannisG10@users.noreply.github.com>
2024-11-06 13:19:49 +0100

    Update searchInsert.py

--- LeetCode-solution/searchInsert/searchInsert.py
+++ LeetCode-solution/searchInsert/searchInsert.py
@@ -1,1 +1,20 @@
+class Solution:
+    def searchInsert(self,nums,target):
+        left = 0
+        right = len(nums) - 1
+        
+        while left <= right:
+            midlle = (left+right)//2
 
+            if nums[midlle] == target:
+                return midlle
+            elif nums[midlle] < target:
+                left = midlle + 1
+            else:
+                right = midlle - 1
+
+        return left
+
+
+
+        
