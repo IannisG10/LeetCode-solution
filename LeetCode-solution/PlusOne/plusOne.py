2f5414967ad31cb80361bc0c5884656e070aae86
IannisG10 <144343272+IannisG10@users.noreply.github.com>
2024-11-08 13:14:21 +0100

    Update plusOne.py

--- LeetCode-solution/PlusOne/plusOne.py
+++ LeetCode-solution/PlusOne/plusOne.py
@@ -1,3 +1,12 @@
 class Solution:
   def PlusOne(self,digits):
     l = len(digits) - 1
+    for i in range(l,-1,-1):
+            digits[i] += 1
+            if digits[i]<= 9:
+                return digits
+            else:
+                digits[i] = 0
+
+            if digits[0] == 0:
+                return [1] + digits
