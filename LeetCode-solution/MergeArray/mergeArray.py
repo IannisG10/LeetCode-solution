34c9721abc08ddf1e7cb1c37704958fb328b499c
IannisG10 <144343272+IannisG10@users.noreply.github.com>
2024-11-13 14:35:12 +0300

    Update mergeArray.py

--- LeetCode-solution/MergeArray/mergeArray.py
+++ LeetCode-solution/MergeArray/mergeArray.py
@@ -1,4 +1,20 @@
 class Solution:
-  def mergeArray(self,nums1,nums2):
-    m = len(nums1) - 1
-    n = len(nums2)
+  def mergeArray(self,nums1,nums2,m,n):
+    while nums1 and nums1[-1] == 0:
+      nums1.pop()
+
+    for i in range(n):
+      nums1.append(nums2[i])
+
+    nums1.sort()
+
+array1 = [1,2,3,4,0,0]
+array2 = [2,4,5,6]
+
+m = len(array1)
+n = len(array2)
+
+Solution().mergeArray(array1,array2,m,n)
+print(array1)
+      
+    
