5b86fabcaa98ea7a50ef0ab4344b044b91b698d1
IannisG10 <144343272+IannisG10@users.noreply.github.com>
2024-11-18 09:38:28 +0100

    Update arrayToBST.py

--- LeetCode-solution/ArrayToBST/arrayToBST.py
+++ LeetCode-solution/ArrayToBST/arrayToBST.py
@@ -1,1 +1,10 @@
+class TreeNode:
+  # constructor of the Node
+  def __init__(self,value):
+    self.value = value
+    self.left = left
+    self.right = right
 
+class insertIntoBST:
+  def arrayToBST(self,array):
+    
