34b8a7659392e2202db38f9b0f821b7ee949a29b
IannisG10 <144343272+IannisG10@users.noreply.github.com>
2024-11-20 09:03:59 +0100

    Update PascalTriangle_II.py

--- LeetCode-solution/Pascal's Triangle II/PascalTriangle_II.py
+++ LeetCode-solution/Pascal's Triangle II/PascalTriangle_II.py
@@ -1,1 +1,14 @@
+class Solution(object):
+    def getRow(self, rowIndex):
+        if rowIndex == 0:
+            return [1]
 
+        previous_row = self.getRow(rowIndex-1)
+
+        current_row = [1]
+        for i in range(1,len(previous_row)):
+            current_row.append(previous_row[i] + previous_row[i-1])
+
+        current_row.append(1)
+        return current_row
+        
