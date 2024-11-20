d64bac79df991edc774605ceb3b6fd8ef9377c07
IannisG10 <144343272+IannisG10@users.noreply.github.com>
2024-11-20 07:21:08 +0100

    Update example.py

--- LeetCode-solution/Pascal's Triangle/example.py
+++ LeetCode-solution/Pascal's Triangle/example.py
@@ -1,1 +1,27 @@
+class Example:
+    def generate(self, numRows):
 
+        if numRows == 0:
+            return []
+        if numRows == 1:
+            return [[1]]
+
+        pascal_triangle = self.generate(numRows - 1)
+        previous_row = pascal_triangle[-1]
+        len_row = len(previous_row)
+
+        current_row = [1]
+        
+        for index in range(1,len_row):
+            current_row.append(previous_row[index] + previous_row[index-1])
+
+        current_row.append(1)
+        pascal_triangle.append(current_row)
+
+        return pascal_triangle
+    
+    
+row = 4
+TP = Example().generate(row)
+
+print(TP)
