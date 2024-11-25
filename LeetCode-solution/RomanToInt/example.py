4bee2697011ff0813801d2461b854227d01aa1fb
IannisG10 <144343272+IannisG10@users.noreply.github.com>
2024-11-25 07:33:48 +0100

    Update example.py

--- LeetCode-solution/RomanToInt/example.py
+++ LeetCode-solution/RomanToInt/example.py
@@ -1,1 +1,19 @@
-
+class Solution:
+  def romanToInt(self,s):
+    #Declare a dictionnary to store the common romans number
+    romans = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+    num = 0
+    #Browse the romans numbers with loop
+    for i in range(len(s) - 1):
+      #Si le chiffre romain en cours est inférieur au chiffre romain qui le suit,on soustrait
+      if romans[s[i]] < romans[s[i + 1]]:
+        num -= romans[s[i]]
+      else:
+        num += romans[s[i]]
+        
+    num += romans[s[-1]]
+    
+    return num
+    
+romToINT = Solution().romanToInt("XXXIV")
+print("Le chiffre est:",romToINT)
