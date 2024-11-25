3d5b6b4cf5a8fc79aa7c0efac391d16c56285ab2
IannisG10 <144343272+IannisG10@users.noreply.github.com>
2024-11-25 07:14:08 +0100

    Update romanToInt.py

--- LeetCode-solution/RomanToInt/romanToInt.py
+++ LeetCode-solution/RomanToInt/romanToInt.py
@@ -1,1 +1,13 @@
+class Solution(object):
+    def romanToInt(self, s):
+        dict_romans = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        res = 0
+        for i in range(len(s) - 1):
+            if dict_romans[s[i]]< dict_romans[s[i + 1]]:
+                res -= dict_romans[s[i]]
+            else:
+                res += dict_romans[s[i]]
 
+        res += dict_romans[s[-1]]
+        return res 
+        
